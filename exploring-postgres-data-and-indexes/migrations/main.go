45626ee43ff438edde81b827c75ec66840aed783
MatteoGioioso <gioioso.matteo@gmail.com>
2020-10-07 08:12:21 +0700

    remove old instance uri

--- exploring-postgres-data-and-indexes/migrations/main.go
+++ exploring-postgres-data-and-indexes/migrations/main.go
@@ -7,19 +7,15 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-const rdsInstance = "postgres://madeo:vba25tlm2@pa14sia00icfagv.cndlk5sduxcf.ap-southeast-1.rds.amazonaws.com:5432/pgtrigger"
-const localInstance = "postgres://postgres:123@localhost:5432/test?sslmode=disable"
+const uri = "postgres://postgres:123@localhost:5432/test?sslmode=disable"
 
 func main() {
-	m, err := migrate.New(
-		"file://files",
-		rdsInstance)
+	m, err := migrate.New("file://files", uri)
 	fmt.Println("Connection error: ", err)
 	//err3 := m.Force(2)
 	//fmt.Println(err3)
 
 	err2 := m.Up()
-
 
 	fmt.Println("Migration error:  ", err2)
 }
