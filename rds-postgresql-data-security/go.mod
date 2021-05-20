2dc38bfd4d9b094c5779762eccab758fc0897d98
MatteoGioioso <gioioso.matteo@gmail.com>
2021-05-20 11:30:34 +0700

    add tls config for go

---
+++ rds-postgresql-data-security/go.mod
@@ -0,0 +1,7 @@
+module rds-postgresql-data-security
+
+go 1.14
+
+require (
+	github.com/jackc/pgx/v4 v4.9.0
+)
---
+++ rds-postgresql-data-security/tls-only-config.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"io/ioutil"
+	"os"
+)
+
+// You need to pass the pool configuration to this method
+// In thi case I am using pgx
+func getTSLConfig(config *pgxpool.Config) error {
+	// Get the certificate file
+	certPath := "/certs/rds-combined-ca-bundle.pem"
+	caCert, err := ioutil.ReadFile(certPath)
+	if err != nil {
+		return err
+	}
+
+	// Alternatively you can use x509.SystemCertPool()
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Initialize the tsl config
+	tlsConfig := &tls.Config{}
+	// The server name is the RDS endpoint,
+	// should be something like: <name>.<id>.<aws_region>.rds.amazonaws.com
+	// You can find the endpoint in the RDS console
+	tlsConfig.ServerName = os.Getenv("POSTGRES_HOST")
+	tlsConfig.RootCAs = caCertPool
+
+	// Attach the tsl config to the pool
+	config.ConnConfig.TLSConfig = tlsConfig
+	return nil
+}
