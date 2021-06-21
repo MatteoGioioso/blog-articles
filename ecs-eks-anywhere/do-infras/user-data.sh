7a26f2abe340c33e3444c8bc7638456d4c9b5449
MatteoGioioso <gioioso.matteo@gmail.com>
2021-06-21 14:02:20 +0700

    add remove activation keys

--- ecs-eks-anywhere/do-infras/user-data.sh
+++ ecs-eks-anywhere/do-infras/user-data.sh
@@ -11,3 +11,4 @@ export CLUSTER_NAME=ecs-anywhere-test-cluster
 
 curl --proto "https" -o "/tmp/ecs-anywhere-install.sh" "https://amazon-ecs-agent.s3.amazonaws.com/ecs-anywhere-install-latest.sh"
 sudo bash /tmp/ecs-anywhere-install.sh --region "$REGION" --cluster "$CLUSTER_NAME" --activation-id "$ACTIVATION_ID" --activation-code "$ACTIVATION_CODE"
+rm ssm-activation.json
